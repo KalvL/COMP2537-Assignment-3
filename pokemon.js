cd8e6dec6d4c7646c8652a70515a412c37a7a082
Kalvin <kleung204@my.bcit.ca>
2023-05-11 18:22:47 -0700

    Imported the pokemon API

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,15 @@
-const setup = () => {
-  console.log("Hello World!");
-};
+const setup = async () => {
+  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=810');
+  console.log(response.data.results);
+
+  const pokemon = response.data.results;
+  $('#pokemon').empty();
+  var newList = $('<ol></ol>');
+  for (let i = 0; i < pokemon.length; i++) {
+    newList.append(`<li>${pokemon[i].name}</li>`);
+  }
+  $('#pokemon').append(newList);
+
+}
 
 $(document).ready(setup);
